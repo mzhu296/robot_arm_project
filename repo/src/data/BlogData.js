0a5ed568a1f00df8194107fcc91839e658068639
mzhu296 <mzhu296@uwo.ca>
2024-12-02 21:54:41 -0500

    redesign_framework

---
+++ repo/src/data/BlogData.js
@@ -0,0 +1,13 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"",
+        tags:[""],
+        date:"13 May, 2021",
+        imgSrc:"",
+        link:""
+    },
+  
+] 
---
+++ repo/src/data/WorkData.js
@@ -0,0 +1,14 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"",
+        description:"",
+        tags:[""],
+        
+        demo:"",
+        github:""
+    }, 
+  
+] 
