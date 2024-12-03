22d30961bbd68ac11daf2397e51a0fe82c16a7c1
mzhu296 <mzhu296@uwo.ca>
2024-12-03 13:49:18 -0500

    image

--- repo/src/data/ConfigurationData.js
+++ repo/src/data/ConfigurationData.js
@@ -1,5 +1,4 @@
 
-import bottomImage from '../assets/Images/bottom.png';
 
 export const Configurations = [
     {
@@ -7,8 +6,15 @@ export const Configurations = [
         name:"xx",
         tags:["xx"],
         date:"13 May, 2021",
-        imgSrc: bottomImage,
+        imgSrc:"../assets/Images/bottom.png",
         link:"xx"
     },
-  
+    {
+        id:2,
+        name:"xx",
+        tags:["xx"],
+        date:"13 May, 2021",
+        imgSrc:"../assets/Images/joint1.png",
+        link:"xx"
+    },
 ] 
