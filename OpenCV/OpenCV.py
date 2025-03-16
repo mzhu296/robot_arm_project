199c15f55bbe3bdd6c10825b19fbc265a1ccc829
Paul Gherghel <pagherghel@yahoo.com>
2025-03-16 14:20:56 -0400

    Add object boundary drawing for detected contours in OpenCV

--- OpenCV/OpenCV.py
+++ OpenCV/OpenCV.py
@@ -53,6 +53,17 @@ while True:
     # Detect faces in the frame
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     
+    # Draw objects boundaries
+    for cnt in contours:
+        # Get rect
+        rect = cv2.minAreaRect(cnt)
+        (x, y), (w, h), angle = rect
+        # Display rectangle
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        cv2.circle(img, (int(x), int(y)), 5, (0, 0, 255), -1)
+        cv2.polylines(img, [box], True, (255, 0, 0), 2)
+        
     # Loop over the detected faces
     for (x, y, w, h) in faces:
         # Calculate the distance to the face
